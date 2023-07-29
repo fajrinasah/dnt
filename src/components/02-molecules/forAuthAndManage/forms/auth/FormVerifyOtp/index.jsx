f50b3e38b4a64f3d08496958080ddce178a15317
fajrinasah <fajrinasah@gmail.com>
2023-07-30 01:03:10 +0700

    CREATE: verify OTP token feature

--- src/components/02-molecules/forAuthAndManage/forms/auth/FormVerifyOtp/index.jsx
+++ src/components/02-molecules/forAuthAndManage/forms/auth/FormVerifyOtp/index.jsx
@@ -1,17 +1,22 @@
 import InputText from "../../../../../01-atoms/forAuthAndManage/inputs/formInputs/InputText";
 import InputSubmit from "../../../../../01-atoms/forAuthAndManage/inputs/formInputs/InputSubmit";
+import ModalDefaultText from "../../../../../01-atoms/forAuthAndManage/texts/ModalDefaultText";
+
+import "./styles.css";
 
 export default function FormVerifyOtp({
   onSubmit = () => {},
   handleBlur = () => {},
   handleChange = () => {},
   isSubmitting = () => {},
+  otpTouched,
+  otpErrors,
   otpValue,
 }) {
   return (
     <form onSubmit={onSubmit} className="form verify d-flex-col">
       <InputText
-        flexDirection="column"
+        flexDirection="row"
         color="accent"
         inputId="token"
         labelText="OTP token"
@@ -22,6 +27,16 @@ export default function FormVerifyOtp({
         onBlur={handleBlur}
         onChange={handleChange}
       />
+
+      {otpTouched && otpErrors && (
+        <div className="error-container otp d-flex-row">
+          <ModalDefaultText
+            content={otpErrors}
+            color="contrast"
+            bgColor="accent"
+          />
+        </div>
+      )}
 
       <InputSubmit value="Verify" disabled={isSubmitting} width="auto" />
     </form>
