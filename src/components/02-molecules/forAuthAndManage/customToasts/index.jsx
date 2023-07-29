b637ebdd399ab961ae00efd74b410f3b43fe9060
fajrinasah <fajrinasah@gmail.com>
2023-07-29 19:33:46 +0700

    UPDATE: custom toasts

--- src/components/02-molecules/forAuthAndManage/customToasts/index.jsx
+++ src/components/02-molecules/forAuthAndManage/customToasts/index.jsx
@@ -5,42 +5,50 @@ import {
   faCircleXmark,
 } from "@fortawesome/free-solid-svg-icons";
 
-import "./styles.css";
+import ModalDefaultText from "../../../01-atoms/forAuthAndManage/texts/ModalDefaultText";
 
 export function toastBlank(content = "") {
   toast.custom(
-    <div className="modal-default-text toast d-flex-row color-contrast bg-main bold">
-      <div className="frame">
-        <div className="modal-content d-flex-row">
-          <p>{content}</p>
-        </div>
-      </div>
-    </div>
+    <ModalDefaultText
+      type="toast"
+      color="contrast"
+      bgColor="main"
+      bold=""
+      content={<p>{content}</p>}
+    />
   );
 }
 
 export function toastSuccess(content = "") {
   toast.custom(
-    <div className="modal-default-text toast d-flex-row color-main bg-contrast bold">
-      <div className="frame">
-        <div className="modal-content d-flex-row">
+    <ModalDefaultText
+      type="toast"
+      color="contrast"
+      bgColor="accent"
+      bold=""
+      content={
+        <div>
           <FontAwesomeIcon icon={faCircleCheck} className="toast-icon" />
           <p>{content}</p>
         </div>
-      </div>
-    </div>
+      }
+    />
   );
 }
 
 export function toastError(content = "") {
   toast.custom(
-    <div className="modal-default-text toast d-flex-row color-main bg-accent bold">
-      <div className="frame">
-        <div className="modal-content d-flex-row">
+    <ModalDefaultText
+      type="toast"
+      color="main"
+      bgColor="warning"
+      bold="bold"
+      content={
+        <div>
           <FontAwesomeIcon icon={faCircleXmark} className="toast-icon" />
           <p>{content}</p>
         </div>
-      </div>
-    </div>
+      }
+    />
   );
 }
