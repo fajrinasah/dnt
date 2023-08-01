429763dd8b0f40d71642b3653b067dfdf0365537
fajrinasah <fajrinasah@gmail.com>
2023-08-01 17:08:20 +0700

    UPDATE: using GuidesSelectMultiple component

--- src/components/02-molecules/forAuthAndManage/forms/products/FormAddProduct/index.jsx
+++ src/components/02-molecules/forAuthAndManage/forms/products/FormAddProduct/index.jsx
@@ -9,6 +9,7 @@ import ButtonStandard from "../../../../../01-atoms/forAuthAndManage/buttons/But
 import "./styles.css";
 import ModalDefaultText from "../../../../../01-atoms/forAuthAndManage/texts/ModalDefaultText";
 import InputFileCustom from "../../../../../01-atoms/forAuthAndManage/inputs/formInputs/InputFileCustom";
+import GuidesSelectMultiple from "../../../../../01-atoms/forAuthAndManage/texts/GuidesSelectMultiple";
 
 export default function FormAddProduct({
   categoriesArr = [],
@@ -64,69 +65,7 @@ export default function FormAddProduct({
       )}
 
       <div className="multi-select-container d-flex-col">
-        <details className="guides-dropdown">
-          <summary>Guides on how to select multiple categories</summary>
-
-          <details>
-            <summary>Guides for mouse users</summary>
-            <p>
-              Mouse users can hold the <span className="text-button">Ctrl</span>
-              , <span className="text-button">Command</span>, or{" "}
-              <span className="text-button">Shift</span> keys (depending on what
-              makes sense for your operating system) and then click multiple
-              options to select/deselect them.
-            </p>
-          </details>
-
-          <details>
-            <summary>Guides for keyboard users</summary>
-
-            <details>
-              <summary>Select multiple contiguous items by:</summary>
-              <ul>
-                <li>
-                  Focusing on the category option (e.g. using{" "}
-                  <span className="text-button">Tab</span> ).
-                </li>
-                <li>
-                  Selecting an item at the top or bottom of the range you want
-                  to select using the <span className="text-button">Up</span>{" "}
-                  and <span className="text-button">Down</span> cursor keys to
-                  go up and down the options.
-                </li>
-                <li>
-                  Holding down the <span className="text-button">Shift</span>{" "}
-                  key and then using the <span className="text-button">Up</span>{" "}
-                  and <span className="text-button">Down</span> cursor keys to
-                  increase or decrease the range of items selected.
-                </li>
-              </ul>
-            </details>
-
-            <details>
-              <summary>Select multiple non-contiguous items by:</summary>
-              <ul>
-                <li>
-                  Focusing on the category option (e.g. using{" "}
-                  <span className="text-button">Tab</span> ).
-                </li>
-                <li>
-                  Holding down the <span className="text-button">Ctrl</span> key
-                  then using the <span className="text-button">Up</span> and
-                  <span className="text-button">Down</span> cursor keys to
-                  change the "focused" select option, i.e. the one that will be
-                  selected if you choose to do so. The "focused" select option
-                  is highlighted with a dotted outline, in the same way as a
-                  keyboard-focused link.
-                </li>
-                <li>
-                  Pressing <span className="text-button">Space</span> to
-                  select/deselect "focused" select options.
-                </li>
-              </ul>
-            </details>
-          </details>
-        </details>
+        <GuidesSelectMultiple />
 
         <InputSelect
           flexDirection="row"
@@ -215,20 +154,20 @@ export default function FormAddProduct({
         </div>
       )}
 
-      <div className="input-submit-container d-flex-row">
+      <div className="input-submit-container d-flex-col">
+        <InputSubmit
+          value="Save"
+          disabled={disableSave}
+          story="raised-main"
+          width="full"
+        />
+
         <ButtonStandard
           story="flat"
           bold=""
           width="full"
           content="Cancel"
           onClick={cancelHandler}
-        />
-
-        <InputSubmit
-          value="Save"
-          disabled={disableSave}
-          story="raised-main"
-          width="full"
         />
       </div>
     </form>
